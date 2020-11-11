40ed58f30f9c17c2a6fa18827733617c24daad2f
TeukkaK <48186687+TeukkaK@users.noreply.github.com>
2020-11-11 13:21:35 +0200

    Add files via upload

---
+++ build/precache-manifest.b0dc976865c76749ea55e3aeaf499460.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "341e656cc5375794158ef32a5ad004ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eeef0d620cd8e6a96e1c",
+    "url": "/static/css/main.d0a785c9.chunk.css"
+  },
+  {
+    "revision": "39b3c5ab26027a3c2ce6",
+    "url": "/static/js/2.bb086d9f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bb086d9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eeef0d620cd8e6a96e1c",
+    "url": "/static/js/main.e352d108.chunk.js"
+  },
+  {
+    "revision": "f326c947c2bb131ebc6d",
+    "url": "/static/js/runtime-main.7900c9ab.js"
+  }
+]);
