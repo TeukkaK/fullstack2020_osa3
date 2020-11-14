334e4b8a6fb6831c658f831058974daf9b6284db
Teemu Korhonen <teemu.m.korhonen@student.jyu.fi>
2020-11-14 16:49:13 +0200

    'juu'

--- mongo.js
+++ mongo.js
@@ -6,9 +6,8 @@ if (process.argv.length<3) {
 }
 
 const password = process.argv[2]
-const db = pluettelo 
 
-const url = `mongodb+srv://fullstack:${password}@pluettelo.ofp0w.mongodb.net/${db}?retryWrites=true&w=majority`
+const url = `mongodb+srv://fullstack:${password}@pluettelo.ofp0w.mongodb.net/pluettelo?retryWrites=true&w=majority`
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true})
 
