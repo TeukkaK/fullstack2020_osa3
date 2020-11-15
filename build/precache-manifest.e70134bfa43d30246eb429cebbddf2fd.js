01c77254aa4f8817c653cec04d0c4c8c1a3032d1
Teemu Korhonen <teemu.m.korhonen@student.jyu.fi>
2020-11-15 16:10:23 +0200

    'ret'

---
+++ build/precache-manifest.e70134bfa43d30246eb429cebbddf2fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c40f0efe6278523906b54a85cc674520",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b1f4e7cfbeca1532ee7",
+    "url": "/static/css/main.d0a785c9.chunk.css"
+  },
+  {
+    "revision": "39b3c5ab26027a3c2ce6",
+    "url": "/static/js/2.bb086d9f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bb086d9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b1f4e7cfbeca1532ee7",
+    "url": "/static/js/main.b9e23f28.chunk.js"
+  },
+  {
+    "revision": "f326c947c2bb131ebc6d",
+    "url": "/static/js/runtime-main.7900c9ab.js"
+  }
+]);
